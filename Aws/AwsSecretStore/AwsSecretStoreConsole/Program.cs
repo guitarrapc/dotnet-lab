ed39ef6655ebc044d8925f8c62aa09a4ceb0ea8c
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-04-19 23:20:58 +0900

    feat: add aws secret sample

---
+++ Aws/AwsSecretStore/AwsSecretStoreConsole/Program.cs
@@ -0,0 +1,96 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using System;
+using System.IO;
+
+namespace AwsSecretTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"secret: {GetSecret()}");
+        }
+
+        public static string GetSecret()
+        {
+            string secretName = "your_super_secret";
+            string region = "ap-northeast-1";
+            string secret = "";
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            // client won't need resion if run from ec2/ecs/lambda
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+            //IAmazonSecretsManager client = new AmazonSecretsManagerClient();
+
+            GetSecretValueRequest request = new GetSecretValueRequest();
+            request.SecretId = secretName;
+            request.VersionStage = "AWSCURRENT"; // VersionStage defaults to AWSCURRENT if unspecified.
+
+            GetSecretValueResponse response = null;
+
+            // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+            // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            // We rethrow the exception by default.
+
+            try
+            {
+                response = client.GetSecretValueAsync(request).Result;
+            }
+            catch (DecryptionFailureException e)
+            {
+                // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (InternalServiceErrorException e)
+            {
+                // An error occurred on the server side.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (InvalidParameterException e)
+            {
+                // You provided an invalid value for a parameter.
+                // Deal with the exception here, and/or rethrow at your discretion
+                throw;
+            }
+            catch (InvalidRequestException e)
+            {
+                // You provided a parameter value that is not valid for the current state of the resource.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (ResourceNotFoundException e)
+            {
+                // We can't find the resource that you asked for.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (System.AggregateException ae)
+            {
+                // More than one of the above exceptions were triggered.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+
+            // Decrypts secret using the associated KMS CMK.
+            // Depending on whether the secret is a string or binary, one of these fields will be populated.
+            if (response.SecretString != null)
+            {
+                secret = response.SecretString;
+            }
+            else
+            {
+                memoryStream = response.SecretBinary;
+                StreamReader reader = new StreamReader(memoryStream);
+                string decodedBinarySecret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
+            }
+
+            // Your code goes here.    
+            return secret;
+        }
+    }
+}
