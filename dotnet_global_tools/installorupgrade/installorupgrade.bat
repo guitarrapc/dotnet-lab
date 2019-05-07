beb57a716e58332249f4f079698c3cbcc4aca9bc
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-05-07 11:54:24 +0900

    fix: typo

--- dotnet_global_tools/installorupgrade/installorupgrade.bat
+++ dotnet_global_tools/installorupgrade/installorupgrade.bat
@@ -1,6 +1,6 @@
 dotnet tool list -g | findstr unitybuildrunner
 if '%errorlevel%' EQU '0' (
-    otnet tool update --global UnityBuildRunner     
+    dotnet tool update --global UnityBuildRunner
 ) else (
     dotnet tool install --global UnityBuildRunner --version 1.1.7
 )
