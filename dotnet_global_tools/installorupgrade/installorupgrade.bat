856f803730af50c7306f834506d32edfdecbf613
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-05-07 11:52:05 +0900

    fix: mistake cmd if

--- dotnet_global_tools/installorupgrade/installorupgrade.bat
+++ dotnet_global_tools/installorupgrade/installorupgrade.bat
@@ -1,6 +1,6 @@
 dotnet tool list -g | findstr unitybuildrunner
-if ('%errorlevel%' == '1') (
-    dotnet tool install --global UnityBuildRunner --version 1.1.5
+if '%errorlevel%' EQU '0' (
+    otnet tool update --global UnityBuildRunner     
 ) else (
-    dotnet tool update --global UnityBuildRunner
+    dotnet tool install --global UnityBuildRunner --version 1.1.7
 )
