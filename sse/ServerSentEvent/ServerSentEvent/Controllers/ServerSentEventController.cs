9931747cf28cc76c354f1b7854333fae36ce1488
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-06-03 18:42:23 +0900

    feat: add Server Sent Event sample

---
+++ sse/ServerSentEvent/ServerSentEvent/Controllers/ServerSentEventController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerSentEvent.Controllers
+{
+    /// <summary>
+    /// Use Controller instead of Middleware
+    /// </summary>
+    [Route("sse")]
+    public class ServerSentEventController : ControllerBase
+    {
+        [HttpGet]
+        public async Task Get()
+        {
+            var response = Response;
+            response.Headers.Add("Content-Type", "text/event-stream");
+            for (var i = 0; true; ++i)
+            {
+                await response.WriteAsync($"data: Controller {i} at {DateTime.Now}\n\n");
+
+                await response.Body.FlushAsync();
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
