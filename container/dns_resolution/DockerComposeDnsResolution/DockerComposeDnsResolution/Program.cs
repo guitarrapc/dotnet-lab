c2e3486d1c3cccc9c5990ebc1d6a2de9a1119478
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-06-28 11:19:28 +0900

    chore: add exception memo

--- container/dns_resolution/DockerComposeDnsResolution/DockerComposeDnsResolution/Program.cs
+++ container/dns_resolution/DockerComposeDnsResolution/DockerComposeDnsResolution/Program.cs
@@ -47,7 +47,8 @@ namespace DockerComposeDnsResolution
             var host = resHost?.FirstOrDefault();
             this.Context.Logger.LogInformation($"resolution count: {resHost?.Length ?? 0}, {nameof(hostAddress)}: {host}");
 
-            // MEMO: SHOULD NOT PASS URL. only host is available
+            //// MEMO: SHOULD NOT PASS URL. only host is available
+            //// System.Net.Internals.SocketExceptionFactory+ExtendedSocketException (00000005, 6): No such device or address
             //var resUrl = await Dns.GetHostAddressesAsync(url);
             //this.Context.Logger.LogInformation($"resolution {nameof(url)}: {resUrl}");
 
