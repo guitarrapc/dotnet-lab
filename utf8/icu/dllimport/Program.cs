b22bc935686f62c6adca23f07ef2e3c7ecd5c6f2
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-08-01 19:12:45 +0900

    feat: icu dllimport on windows, linux

--- utf8/icu/dllimport/Program.cs
+++ utf8/icu/dllimport/Program.cs
@@ -1,47 +1,22 @@
-using icusample.Interop;
 using System;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApp4
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //var length = TextLengthInGrapheme("hogemoge");
-            GetIcuText("👪");
-            //Console.WriteLine(length);
-        }
+        // windows. you can try `icuuc` without file extension.
+        [DllImport("icuuc.dll")]
+        // linux -> entrypoint not found..... (try some method that is `extern "C"`.....)
+        //[DllImport("libicuuc.so.57.1", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double u_getNumericValue(int c);
 
-        public static int TextLengthInGrapheme(string text)
+        static void Main()
         {
-            //TODO: won't work....
-            var iterator = Icuuc.ubrk_open(UBreakIteratorType.UBRK_CHARACTER, Icuuc.uloc_getDefault(), null, 0, out _);
-            var utext = Icuuc.utext_openUChars(IntPtr.Zero, text, text.Length, out _);
-            Icuuc.ubrk_setUText(iterator, utext, out _);
-            var n = 0;
-            while (Icuuc.ubrk_next(iterator) != Icuuc.UBRK_DONE) n++;
-            Icuuc.ubrk_close(iterator);
-            Icuuc.utext_close(utext);
-            return n;
-        }
-
-        public static void GetIcuText(string text)
-        {
-            //TODO: won't work....
-            var iterator = Icuuc.ubrk_open(UBreakIteratorType.UBRK_CHARACTER, Icuuc.uloc_getDefault(), null, 0, out _);
-            var utext = Icuuc.utext_openUChars(IntPtr.Zero, text, text.Length, out _);
-            Icuuc.ubrk_setUText(iterator, utext, out _);
-            int current = Icuuc.ubrk_current(iterator);
-            while (current != Icuuc.UBRK_DONE)
-            {
-                int next = Icuuc.ubrk_next(iterator);
-                if (next == Icuuc.UBRK_DONE) break;
-                int size = next - current;
-                Console.WriteLine($"{current}-{next}: {text.Substring(current, size)}");
-                current = next;
-            }
-            Icuuc.ubrk_close(iterator);
-            Icuuc.utext_close(utext);
+            Console.WriteLine(u_getNumericValue('Ⅱ'));
+            Console.WriteLine(u_getNumericValue('ⅱ'));
+            Console.WriteLine(u_getNumericValue('٢'));
+            Console.WriteLine(u_getNumericValue('二'));
         }
     }
 }
