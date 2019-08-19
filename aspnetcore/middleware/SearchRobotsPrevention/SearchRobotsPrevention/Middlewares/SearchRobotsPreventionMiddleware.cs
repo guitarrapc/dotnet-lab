a00fd176857162b1312ca3f3d3f977610daca4db
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-08-19 09:37:34 +0900

    feat: add asp.net core search prevention middleware

---
+++ aspnetcore/middleware/SearchRobotsPrevention/SearchRobotsPrevention/Middlewares/SearchRobotsPreventionMiddleware.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace SearchRobotsPrevention.Middleware
+{
+    public static class SearchRobotsPreventionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSearchRobotsPrevention(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SearchRobotsPreventionMiddleware>();
+        }
+    }
+
+    public class SearchRobotsPreventionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _maxAge = $"max-age={TimeSpan.FromDays(1).TotalMilliseconds}";
+
+        public SearchRobotsPreventionMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/robots.txt"))
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.Headers.Add("Cache-Control", _maxAge);
+
+                await context.Response.WriteAsync("User-Agent: *\r\nDisallow: /");
+            }
+            else
+            {
+                await this._next(context);
+            }
+        }
+    }
+}
---
+++ aspnetcore/middleware/SearchRobotsPrevention/SearchRobotsPrevention/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SearchRobotsPrevention.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
