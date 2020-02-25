002fdedf4352e732b025e384811b2f48f1364f23
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-02-25 20:15:16 +0900

    feat: get dotnet process from named pipe

---
+++ diagnostics/IpcClient/DiagnosticCore/DiagnosticClient.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DiagnosticCore
+{
+    public class DiagnosticClient
+    {
+        public static IEnumerable<int> GetPublishedProcessIds()
+            => Directory.GetFiles(IpcClient.IpcRootPath)
+                    .Select(x => new FileInfo(x).Name)
+                    .Where(x => Regex.IsMatch(x, IpcClient.DiagnosticsPortPattern))
+                    .Select(x => int.Parse(Regex.Match(x, IpcClient.DiagnosticsPortPattern).Groups[1].Value, NumberStyles.Integer))
+                    .Distinct();
+
+        public static IEnumerable<Process> GetAttachableProcesses()
+            => DiagnosticClient.GetPublishedProcessIds()
+                .Select(x => GetProcessById(x))
+                .Where(x => x != null)
+                .OrderBy(x => x.ProcessName)
+                .ThenBy(x => x.Id);
+
+        private static Process GetProcessById(int processId)
+        {
+            try
+            {
+                return Process.GetProcessById(processId);
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+        }
+    }
+}
