c7ccbd86a6b5931e832414340199ad82fa33db26
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-06-30 18:14:05 +0900

    feat: add OidcCert

---
+++ aws/OidcCert/OidcCert/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OidcCert
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var endpoint = new Uri($"https://oidc.eks.ap-northeast-1.amazonaws.com:443");
+            using (var handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback += (HttpRequestMessage msg, X509Certificate2 cert, X509Chain chain, SslPolicyErrors pollyError) =>
+                {
+                    Console.WriteLine($"{cert.FriendlyName}, {cert.Subject}, {cert.Thumbprint.ToLower()}");
+                    //chain.ChainElements.Dump();
+                    var validcert = chain.ChainElements.Cast<X509ChainElement>()
+                    .Select(item =>
+                    {
+                        Console.WriteLine($"{item.Certificate.FriendlyName}, {item.Certificate.Subject}, {item.Certificate.Thumbprint.ToLower()}, {item.Certificate.NotAfter}");
+                        return item;
+                    })
+                    .Where(x => x.Certificate.Extensions.Cast<X509Extension>().Any(x => x.Critical))
+                    .Reverse() // check chain last
+                    .First();
+
+                    Console.WriteLine($"Result Cert: {validcert.Certificate.FriendlyName}, {validcert.Certificate.Subject}, {validcert.Certificate.Thumbprint.ToLower()}, {validcert.Certificate.NotAfter}");
+                    return chain.Build(cert);
+                };
+
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    using (HttpResponseMessage response = await client.GetAsync(endpoint))
+                    {
+                        // do nothing.
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ aws/OidcCert/README.md
@@ -0,0 +1,18 @@
+## AWS OIDC Cert Certificate & Thumbprint checker
+
+
+local run on Japan, Tokyo.
+
+```csv
+, CN=oidc.eks.ap-northeast-1.amazonaws.com, b715dc079832da5fc1d4706515be48be79a1c871
+, CN=oidc.eks.ap-northeast-1.amazonaws.com, b715dc079832da5fc1d4706515be48be79a1c871, 08/09/2020 21:00:00
+, CN=Amazon, OU=Server CA 1B, O=Amazon, C=US, 917e732d330f9a12404f73d8bea36948b929dffc, 19/10/2025 09:00:00
+, CN=Amazon Root CA 1, O=Amazon, C=US, 06b25927c42a721631c1efd9431e648fa62e1e39, 31/12/2037 10:00:00
+, CN=Starfield Services Root Certificate Authority - G2, O="Starfield Technologies, Inc.", L=Scottsdale, S=Arizona, C=US, 9e99a48a9960b14926bb7f3b02e22da2b0ab7280, 29/06/2034 02:39:16
+Starfield Class 2 Certification Authority, OU=Starfield Class 2 Certification Authority, O="Starfield Technologies, Inc.", C=US, ad7e1c28b064ef8f6003402014c3d0e3370eb58a, 30/06/2034
+02:39:16
+```
+
+```shell
+Result Cert: , CN=Starfield Services Root Certificate Authority - G2, O="Starfield Technologies, Inc.", L=Scottsdale, S=Arizona, C=US, 9e99a48a9960b14926bb7f3b02e22da2b0ab7280, 29/06/2034 02:39:16
+```
