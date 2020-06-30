cb57ffee8c57e315499b9455144bc13380ba9dff
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-06-30 19:23:45 +0900

    feat: add tcp request

--- aws/OidcCert/OidcCert/Program.cs
+++ aws/OidcCert/OidcCert/Program.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Security;
+using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace OidcCert
 {
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var endpoint = new Uri($"https://oidc.eks.ap-northeast-1.amazonaws.com:443");
+            var endpoint = "oidc.eks.ap-northeast-1.amazonaws.com";
+            Console.WriteLine("------ Begin Https Request ----");
+            await DownloadSslCertificateHttp(endpoint);
+
+            Console.WriteLine("------ Begin Tcp Request ----");
+            await DownloadSslCertificateTcp(endpoint);
+        }
+
+        public static async Task DownloadSslCertificateHttp(string endpoint)
+        {
+            var url = new Uri($"https://{endpoint}:443");
             using (var handler = new HttpClientHandler())
             {
                 handler.ServerCertificateCustomValidationCallback += (HttpRequestMessage msg, X509Certificate2 cert, X509Chain chain, SslPolicyErrors pollyError) =>
@@ -34,10 +46,43 @@ namespace OidcCert
 
                 using (HttpClient client = new HttpClient(handler))
                 {
-                    using (HttpResponseMessage response = await client.GetAsync(endpoint))
+                    using (HttpResponseMessage response = await client.GetAsync(url))
                     {
                         // do nothing.
                     }
+                }
+            }
+        }
+
+        public static async Task<X509Certificate2> DownloadSslCertificateTcp(string endpoint)
+        {
+            using (var client = new TcpClient())
+            {
+                client.Connect(endpoint, 443);
+
+                using (var ssl = new SslStream(client.GetStream(), false,
+                    new RemoteCertificateValidationCallback(
+                            (sender, cert, chain, pollyError) =>
+                            {
+                                Console.WriteLine($"{chain.ChainElements.Count} chains, PollyError: {pollyError}, Cert: {cert.Issuer}, {cert.Subject}");
+                                //chain.ChainElements.Dump();
+                                var validcert = chain.ChainElements.Cast<X509ChainElement>()
+                                .Select(item =>
+                                {
+                                    Console.WriteLine($"{item.Certificate.FriendlyName}, {item.Certificate.Subject}, {item.Certificate.Thumbprint.ToLower()}, {item.Certificate.NotAfter}");
+                                    return item;
+                                })
+                                .Where(x => x.Certificate.Extensions.Cast<X509Extension>().Any(x => x.Critical))
+                                .Reverse() // check chain last
+                                .First();
+                                Console.WriteLine($"Result Cert: {validcert.Certificate.FriendlyName}, {validcert.Certificate.Subject}, {validcert.Certificate.Thumbprint.ToLower()}, {validcert.Certificate.NotAfter}");
+                                return true;
+                            }
+                        ), null))
+                {
+                    await ssl.AuthenticateAsClientAsync(endpoint);
+                    var cert = new X509Certificate2(ssl.RemoteCertificate);
+                    return cert;
                 }
             }
         }
