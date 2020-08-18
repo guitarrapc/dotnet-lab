478366959257b820c7654ef56bb4cc8680405637
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-08-18 12:53:22 +0900

    feat(blazor): add todo items page and prop

---
+++ blazor/TodoList/TodoList/Data/TodoItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
